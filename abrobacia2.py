21f40a1563cc2fbd3257ab29a8c09452b0c0ce57
dasha <zhegdasha3@gmail.com>
2026-06-15 03:22:55 +0300

    15.06

--- abrobacia2.py
+++ abrobacia2.py
@@ -31,25 +31,36 @@
 #8,12,13
 # print(int("11111110",2))
 # print(bin(180))
-from string import ascii_lowercase
 
 #14
-def to_n(n,b):
-    s='0123456789'+ascii_lowercase
-    rez=""
-    while n>0:
-        a=s[n%b]
-        rez=a+rez
-        n=n//b
-    return rez
-
-l=120
-print(to_n(l,11))
-
-for x in range(0,3000):
-    t=(9*11**210)+(8*11**150)-x
-    r=to_n(t,11)
-    if r.count("0")==60:
-        print(x)
-
+# from string import ascii_lowercase
+#
+# def to_n(n,b):
+#     s='0123456789'+ascii_lowercase
+#     rez=""
+#     while n>0:
+#         a=s[n%b]
+#         rez=a+rez
+#         n=n//b
+#     return rez
+#
+# l=120
+# print(to_n(l,11))
+#
+# for x in range(0,3000):
+#     t=(9*11**210)+(8*11**150)-x
+#     r=to_n(t,11)
+#     if r.count("0")==60:
+#         print(x)
+#16
+from functools import*
+@lru_cache()
+def f(n):
+    3*g(n-3)+7
+@lru_cache()
+def g(n):
+    if n <=20:
+        return n+2
+    return g(n-3)
+print(f(37811))
 
