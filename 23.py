ce5b5b59a208e03f746aaafc93e5f73b2d163ded
dasha <zhegdasha3@gmail.com>
2026-06-17 15:00:27 +0300

    17.06

--- 23.py
+++ 23.py
@@ -21,14 +21,23 @@
 # print(f(2,15)*f(15,25))
 
 #27314
+# def f(m,n):
+#     if m==n:
+#         return 1
+#     if m>n:
+#         return 0
+#     if m==28:
+#         return 0
+#     if m==36:
+#         return 0
+#     return f(m+1,n)+f(m+5,n)+f(m*3,n)
+# print(f(2,18)*f(18,49)+f(2,30)*f(30,49)-f(2,18)*f(18,30)*f(30,49))
+
+#27310
 def f(m,n):
     if m==n:
         return 1
-    if m>n:
+    if m<n:
         return 0
-    if m==28:
-        return 0
-    if m==36:
-        return 0
-    return f(m+1,n)+f(m+5,n)+f(m*3,n)
-print(f(2,18)*f(18,49)+f(2,30)*f(30,49))
+    return f(m-3,n)+f(m-4,n)+f(m//2,n)
+print(f(78,30)*f(30,2)+f(78,42)*f(42,2)-f(78,42)*f(42,30)*f(30,2))
--- danybait9.py
+++ danybait9.py
@@ -9,17 +9,48 @@
 #                     print(x,y,w,z,sep="  ")
 
 #5
-for n in range(1,130):
-    r=bin(n)[:1]
-    if r.count("11")==1:
-        r=r+"0"
-        r=r.replace(r[:1],"10")
-    else:
-        r=r+"1"
-        r=r.replace(r[:1],"11")
-    m=1
-    if int(r,2)<=1500 and int(r,2)>m:
-        m=r
-        print(n)
+# m=1
+# for n in range(1,1000):
+#     r=bin(n)[2:]
+#     if r.count("11")==1:
+#         r=r+"0"
+#         r=r[2:]
+#         r="10"+r
+#     else:
+#         r=r+"1"
+#         r=r[2:]
+#         r="11"+r
+#     if int(r,2)<=1500 and int(r,2)>m:
+#         m=int(r,2)
+#         nn=n
+# print(m,nn)
+
+
+#6
+# from turtle import*
+# tracer(0)
+# k=30
+# screensize(2000,2000)
+# right(90)
+# for i in range(8):
+#     forward(15*k)
+#     right(60)
+#     forward(10*k)
+#     right(60)
+# up()
+# for x in range(-22,200):
+#     for y in range(-200,20):
+#         goto(x*k,y*k)
+#         dot(3)
+#
+# done()
+
+#8
+# print(int("2000331",6))
+print((4080+16))
+print(2**12)
+print(12*257/8)
+print(386*8388608/1024/1024)
+
 
 
